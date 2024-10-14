339d8e68d639b8a6117da3d82da77652ecf00b0c
bakuwaku <emanukya@terpmail.umd.edu>
2024-10-14 15:57:03 -0400

    Update git2.py

--- git2.py
+++ git2.py
@@ -1,8 +1,11 @@
+# First commit: Added the example function to print course message
 def example():
     return ("This is INST326")
-
+# Storing the return value in a variable
 message= example()
 print (message)
-
+# Second commit: Added say_goodbye function to print a farewell message
 def say_goodbye():
     print("Goodbye")
+# Calling the say_goodbye function
+say_goodbye()
